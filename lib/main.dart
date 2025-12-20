d0587ed19243d36777ec5ee98baa7a0afb65b0ab
Thanh-Thao-1111 <emocho0402@gmail.com>
2025-12-20 11:38:26 +0700

    firebase and fix yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'views/homes/pages/welcome_page.dart';
 import 'package:provider/provider.dart';
 
